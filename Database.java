84ad763f097c5c6fc3adb0b51fe03fd7a4abe0eb
HashimRaza28510 <111235839+HashimRaza28510@users.noreply.github.com>
2022-08-19 20:08:38 +0500

    minor "Comment Changes"

--- Database.java
+++ Database.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package MainFiles;
 import UIUX.Login;
 import UIUX.Main2;
 import java.sql.*;
 import javax.swing.JOptionPane;
-/**
- *
- * @author user
- */
 public class Database {
     
     static Connection conn;
@@ -24,12 +16,7 @@ public class Database {
     static ResultSet rs;
     static Statement stm;
    
-    /**
-     *
-     * @param r1
-     * @param l1
-     */
-   
+  
     public Database() throws ClassNotFoundException, SQLException{
         Class.forName("com.mysql.cj.jdbc.Driver");
         conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/SMDB","root","root");
